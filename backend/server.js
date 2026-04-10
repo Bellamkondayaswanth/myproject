56a8dd9d7d840dd372ec854e215805ea4bd35929
Yaswanth Sai <saiyaswanth429@gmail.com>
2026-04-11 01:42:14 +0530

    fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://golden-fudge-a33fc6.netlify.app'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth',      require('./routes/auth'));
